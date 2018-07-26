ff6561096102187fa4a8827ced9a4fe6f3b25af4
Ralph <ralph.hawkins@digital.cabinet-office.gov.uk>
2018-07-26 16:12:57 +0100

    makes homepage a dashboard

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Report a product safety incident',
+  serviceName: 'Office for Product Safety & Standards',
 
   // Default port that prototype runs on
   port: '3000',
